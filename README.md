11243be65fb4dfb7e9a1c7062955525a65996ff7
Sven <39831639+Svenonlan@users.noreply.github.com>
2021-04-01 22:51:47 +0200

    Update1

--- README.md
+++
@@ -1,1 +0,0 @@
-# Tilburg-RP.nl
---
+++ scripts/form-carousel_fa31.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'Contact formulier',
+    'email_message' => 'Nieuw contact formulier bericht!',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'tilburgrolplay@gmail.com',
+    'to' => 'tilburgrolplay@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 3,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
